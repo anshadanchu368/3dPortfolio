b55a7cf9cedb0da600aba74096497999f98662ad
anshad <anshadanchu368@example.com>
2023-06-22 09:35:59 +0400

    skills

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,11 @@
-import React from "react"
-import { Tilt } from "react-tilt";
+import  Tilt  from "react-parallax-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../style";
 import { fadeIn, textVariant } from "../utils/motion";
 import { services } from "../constants";
-import SectionWrapper from '../hoc/SectionWrapper'
+import SectionWrapper from "../hoc/SectionWrapper";
 
-const ServiceCard = ({ index ,icon,title}) => {
+const ServiceCard = ({ index, icon, title }) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
@@ -18,7 +17,9 @@ const ServiceCard = ({ index ,icon,title}) => {
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
           <img src={icon} alt={title} className="w-16 h-16 object-contain" />
-          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
         </div>
       </motion.div>
     </Tilt>
@@ -56,4 +57,4 @@ const About = () => {
   );
 };
 
-export default SectionWrapper(About,"about")
+export default SectionWrapper(About, "about");
